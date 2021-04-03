fc14538e2311a10a47502fcdffb6dfa7412cf4c9
Jordan Laor <jordanlaor@gmail.com>
2021-04-03 12:01:21 +0300

    trying to make the build work

--- src/App.js
+++ src/App.js
@@ -9,8 +9,13 @@ function App() {
   let [destination, setDestination] = useState([10.292, 47.546]);
   let [userId, setUserId] = useState(null);
   // TODO: Change props to be dynamic, the center should be the chosen destination.
-  // return <TripsSearch frontendtype="tour" zoom={11} center={destination} />;
-  return <SignIn setUserId={setUserId} />;
+  // return ;
+  return (
+    <div>
+      <TripsSearch frontendtype="tour" zoom={11} center={destination} />
+      {/* <SignIn setUserId={setUserId} /> */}
+    </div>
+  );
 }
 
 export default App;
